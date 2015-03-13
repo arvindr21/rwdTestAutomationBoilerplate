d7ab713b621cd48287cbbd1c9cc3c30ba3672e9d
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2015-03-13 09:12:33 +0530

    bug fix

--- test/spec/responsive-Spec.js
+++ test/spec/responsive-Spec.js
@@ -14,30 +14,34 @@ var casper = require("casper").create(),
 casper.start();
 
 casper.test.begin('Check for menu', viewportSizes.length, function suite(test) {
-    casper.each(viewportSizes, function (self, viewportSize, i) {
+    casper.each(viewportSizes, function(self, viewportSize, i) {
         var width = viewportSize[0],
             height = viewportSize[1];
-        
-        casper.wait(5000, function () {
-			
-            this.viewport(width, height);
-            this.echo("width : " + width +" Height: "+height);
 
-            casper.thenOpen(url, function () {	
-                var aTags =  casper.evaluate(function(){
+        casper.wait(5000, function() {
+
+            this.viewport(width, height);
+            this.echo("width : " + width + " Height: " + height);
+
+            casper.thenOpen(url, function() {
+                
+                casper.evaluate(function() {
+                
                     // In here, the context of execution (global) is the same
                     // as if you were at the console for the loaded page
-                    return $('a');
-                });				
-                console.log("a tags "+ aTags.length);
+                    var aList = $('a');
+                    
+                    if (width < 768) {
+                        test.assertVisible('.navbar-toggle');
+                    } else {
+                        test.assertNotVisible('.navbar-toggle');
+                    }
 
-				if (width < 768)
-                    test.assertVisible('.navbar-toggle');
-                else
-                    test.assertNotVisible('.navbar-toggle');
+                    var FPfilename = saveDir + '/fullpage-' + width + ".png";
+                    this.captureSelector(FPfilename, 'body', true);
+                
+                });
 
-                var FPfilename = saveDir + '/fullpage-' + width + ".png";
-                this.captureSelector(FPfilename, 'body', true);
             }).run();
         });
     });
@@ -45,6 +49,6 @@ casper.test.begin('Check for menu', viewportSizes.length, function suite(test) {
 });
 
 
-casper.run(function () {
+casper.run(function() {
     this.echo('Finished captures for ' + url).exit();
 });
